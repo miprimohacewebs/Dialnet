f62d48d2c1b9c9c5fb0dfcbea972340de43ca73e
jmzambrano <zambranosoft@gmail.com>
2017-08-04 21:47:52 +0200

    arreglos panel de administración. Estilos y contenido. Refactorización de nombre de Modelos Creación de vistas

---
+++ Laravel/app/Http/Controllers/AdministracionController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+use App\Publicaciones;
+use App\Categorias;
+use App\Autores;
+/**
+ * Class AdministracionController para controlar la vista de administración.
+ * @package App\Http\Controllers
+ */
+class AdministracionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //Obtiene el número de publicaciones, categorias y autores
+        $publicaciones = Publicaciones::obtenerNumeroPublicaciones();
+        $categorias = Categorias::obtenerNumeroCategorias();
+        $autores = Autores::obtenerNumeroAutores();
+        return view('administracion\administracion', ['publicaciones' => $publicaciones, 'categorias' => $categorias, 'autores' => $autores]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
